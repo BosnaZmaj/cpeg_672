69f67341ac04582baad9a9cb090fbb4ba631f768
BosnaZmaj <indysubasic@gmail.com>
2020-04-29 09:05:16 -0400

    initial commit

---
+++ minesweeper/main.cpp
@@ -0,0 +1,24 @@
+// Indy Subasic
+
+
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <cstdlib>
+#include <time.h>
+
+
+using namespace std;
+
+
+//Global Variables
+
+
+const int x_board_size = 5;
+const int y_board_size = 5;
+
+int number_of_bombs;
+int total_size_of_grid = x_board_size * y_board_size;
+
+
