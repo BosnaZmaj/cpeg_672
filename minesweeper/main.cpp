0336dd6fa41ef62d25669dd48b681b640e9483d8
BosnaZmaj <indysubasic@gmail.com>
2020-04-30 15:59:29 -0400

    completed square logic

--- minesweeper/main.cpp
+++ minesweeper/main.cpp
@@ -29,13 +29,13 @@ bool win_condition;
 
 class board{
 public:
-    bool bomb_is_here;
+    bool bomb_location;
     bool bomb_marked_by_user;
     bool selected_square;
     int neighbor_mine;
     
     board(){
-        bomb_is_here = false;
+        bomb_location = false;
         bomb_marked_by_user = false;
         selected_square = false;
         neighbor_mine = 0;
@@ -55,7 +55,7 @@ void display_game(board new_game[x_board_size][y_board_size]){
         
         for (int column = 0; column < x_board_size; column++){
             
-            if (new_game[row][column].bomb_marked_by_user && new_game[row][column].bomb_is_here){
+            if (new_game[row][column].bomb_marked_by_user && new_game[row][column].bomb_location){
             
                 cout << "!|";
             
@@ -63,7 +63,7 @@ void display_game(board new_game[x_board_size][y_board_size]){
             
                 cout << "?|";
             
-            }else if (new_game[row][column].bomb_is_here){
+            }else if (new_game[row][column].bomb_location){
             
                 if (win_condition or loss_condition){
             
@@ -99,7 +99,7 @@ void end_of_game(board new_game[x_board_size][y_board_size]){
         
         for (int column = 0; column< x_board_size; column++){
             
-            if (new_game[row][column].bomb_is_here){
+            if (new_game[row][column].bomb_location){
                 
                 cout << "@|";
             }else{
@@ -121,7 +121,7 @@ void game_win(board new_game[x_board_size][y_board_size]){
         
         for (int column = 0; column < x_board_size; column++){
             
-            if (new_game[row][column].bomb_is_here) {
+            if (new_game[row][column].bomb_location) {
     
                 cout << "@|";
             } else{
@@ -143,9 +143,9 @@ void bomb_placement(board new_game[x_board_size][y_board_size], int bomb){
         int bomb_row_placement = rand()%x_board_size;
         int bomb_column_placement = rand()%y_board_size;
         
-        if (!new_game[bomb_row_placement][bomb_column_placement].bomb_is_here){
+        if (!new_game[bomb_row_placement][bomb_column_placement].bomb_location){
             
-            new_game[bomb_row_placement][bomb_column_placement].bomb_is_here=true;
+            new_game[bomb_row_placement][bomb_column_placement].bomb_location=true;
         }else{
             
             i--;
@@ -159,3 +159,465 @@ void place_marker(board new_game[x_board_size][y_board_size], int row, int colum
     
 };
 
+void clicked_square(board new_game[x_board_size][y_board_size], int row, int column){
+    
+    if (new_game[row][column].bomb_location){
+        
+        win_condition = false;
+        loss_condition = true;
+        new_game[row][column].selected_square = true;
+        
+        end_of_game(new_game);
+        
+        cout << "BOOOOMMMM!!!!!!!!\n";
+        cout << "SORRY, YOU LOST. PICK UP YOUR PIECES AND RESTART GAME TO PLAY AGAIN!!!\n";
+        cout << endl;
+    
+        int adjacent_mine_count = 0;
+    
+    }
+    
+    else{
+        
+        if (new_game[row][column].bomb_marked_by_user){
+            
+            !new_game[row][column].bomb_marked_by_user;
+        }
+    
+        
+        if (row == 0 && column == 0){
+        
+            if (!new_game[row + 1][column].bomb_location){
+                
+                new_game[row + 1][column].selected_square = true;
+                
+            }else{
+                
+                adjacent_mine_count++;
+            };
+            
+            if (!new_game[row][column + 1].bomb_location){
+                
+                new_game[row][column + 1].selected_square = true;
+            
+            }else{
+                
+                adjacent_mine_count++;
+            };
+            
+            if (!new_game[row + 1][column + 1].bomb_location){
+                
+                new_game[row + 1][column + 1].selected_square = true;
+                
+            }else{
+                
+                adjacent_mine_count++;
+                
+            };
+            
+            if (adjacent_mine_count > 0){
+                
+                new_game[row][column].neighbor_mine = adjacent_mine_count;
+            
+            }
+        }
+        
+        else if (row == x_board_size - 1 && column == y_board_size - 1 ){
+    
+            if (!new_game[row - 1][column].bomb_location){
+        
+                new_game[row - 1][column].selected_square = true;
+        
+            }else{
+        
+                adjacent_mine_count++;
+            };
+    
+            if (!new_game[row][column - 1].bomb_location){
+        
+                new_game[row][column - 1].selected_square = true;
+        
+            }else{
+        
+                adjacent_mine_count++;
+            };
+    
+            if (!new_game[row - 1][column - 1].bomb_location){
+        
+                new_game[row - 1][column - 1].selected_square = true;
+        
+            }else{
+        
+                adjacent_mine_count++;
+        
+            };
+    
+            if (adjacent_mine_count > 0){
+        
+                new_game[row][column].neighbor_mine = adjacent_mine_count;
+        
+            }
+        }
+        
+        else if (row == 0 && column == y_board_size - 1){
+    
+            if (!new_game[row + 1][column].bomb_location){
+        
+                new_game[row + 1][column].selected_square = true;
+        
+            }else{
+        
+                adjacent_mine_count++;
+            };
+    
+            if (!new_game[row][column - 1].bomb_location){
+        
+                new_game[row][column - 1].selected_square = true;
+        
+            }else{
+        
+                adjacent_mine_count++;
+            };
+    
+            if (!new_game[row + 1][column - 1].bomb_location){
+        
+                new_game[row + 1][column - 1].selected_square = true;
+        
+            }else{
+        
+                adjacent_mine_count++;
+        
+            };
+    
+            if (adjacent_mine_count > 0){
+        
+                new_game[row][column].neighbor_mine = adjacent_mine_count;
+        
+            }
+        }
+        
+        else if (column == 0 && row == x_board_size - 1 ){
+    
+            if (!new_game[row][column + 1].bomb_location){
+        
+                new_game[row][column + 1].selected_square = true;
+        
+            }else{
+        
+                adjacent_mine_count++;
+            };
+    
+            if (!new_game[row - 1][column].bomb_location){
+        
+                new_game[row - 1][column].selected_square = true;
+        
+            }else{
+        
+                adjacent_mine_count++;
+            };
+    
+            if (!new_game[row - 1][column + 1].bomb_location){
+        
+                new_game[row - 1][column + 1].selected_square = true;
+        
+            }else{
+        
+                adjacent_mine_count++;
+        
+            };
+    
+            if (adjacent_mine_count > 0){
+        
+                new_game[row][column].neighbor_mine = adjacent_mine_count;
+        
+            }
+        }
+        
+        else if (row == 0){
+    
+            if (!new_game[row + 1][column].bomb_location){
+        
+                new_game[row + 1][column].selected_square = true;
+        
+            }else{
+        
+                adjacent_mine_count++;
+            };
+    
+            if (!new_game[row][column + 1].bomb_location){
+        
+                new_game[row][column + 1].selected_square = true;
+        
+            }else{
+        
+                adjacent_mine_count++;
+            };
+    
+            if (!new_game[row][column - 1].bomb_location){
+        
+                new_game[row][column - 1].selected_square = true;
+        
+            }else{
+        
+                adjacent_mine_count++;
+        
+            }
+            
+            if (!new_game[row + 1][column + 1].bomb_location){
+                
+                new_game[row + 1][column + 1].selected_square = true;
+            
+            }else{
+                
+                adjacent_mine_count++;
+            }
+            
+            if (!new_game[row + 1][column - 1].bomb_location){
+                
+                new_game[row + 1][column -1].selected_square = true;
+            }
+    
+            if (adjacent_mine_count > 0){
+        
+                new_game[row][column].neighbor_mine = adjacent_mine_count;
+        
+            }
+        }
+        
+        else if (column == 0){
+    
+            if (!new_game[row - 1][column].bomb_location){
+        
+                new_game[row - 1][column].selected_square = true;
+        
+            }else{
+        
+                adjacent_mine_count++;
+            };
+    
+            if (!new_game[row + 1][column].bomb_location){
+        
+                new_game[row + 1][column].selected_square = true;
+        
+            }else{
+        
+                adjacent_mine_count++;
+            };
+    
+            if (!new_game[row][column + 1].bomb_location){
+        
+                new_game[row][column + 1].selected_square = true;
+        
+            }else{
+        
+                adjacent_mine_count++;
+        
+            }
+    
+            if (!new_game[row - 1][column + 1].bomb_location){
+        
+                new_game[row - 1][column + 1].selected_square = true;
+        
+            }else{
+        
+                adjacent_mine_count++;
+            }
+    
+            if (!new_game[row + 1][column + 1].bomb_location){
+        
+                new_game[row + 1][column + 1].selected_square = true;
+            }
+    
+            if (adjacent_mine_count > 0){
+        
+                new_game[row][column].neighbor_mine = adjacent_mine_count;
+        
+            }
+            
+        }
+        
+        else if (row == x_board_size - 1){
+    
+            if (!new_game[row - 1][column].bomb_location){
+        
+                new_game[row - 1][column].selected_square = true;
+        
+            }else{
+        
+                adjacent_mine_count++;
+            };
+    
+            if (!new_game[row][column + 1].bomb_location){
+        
+                new_game[row][column + 1].selected_square = true;
+        
+            }else{
+        
+                adjacent_mine_count++;
+            };
+    
+            if (!new_game[row][column - 1].bomb_location){
+        
+                new_game[row][column - 1].selected_square = true;
+        
+            }else{
+        
+                adjacent_mine_count++;
+        
+            }
+    
+            if (!new_game[row - 1][column - 1].bomb_location){
+        
+                new_game[row + 1][column - 1].selected_square = true;
+        
+            }else{
+        
+                adjacent_mine_count++;
+            }
+    
+            if (!new_game[row + 1][column - 1].bomb_location){
+        
+                new_game[row + 1][column -1].selected_square = true;
+            }
+    
+            if (adjacent_mine_count > 0){
+        
+                new_game[row][column].neighbor_mine = adjacent_mine_count;
+        
+            }
+        }
+        
+        else if (column == y_board_size - 1){
+            
+    
+            if (!new_game[row - 1][column].bomb_location){
+        
+                new_game[row - 1][column].selected_square = true;
+        
+            }else{
+        
+                adjacent_mine_count++;
+            };
+    
+            if (!new_game[row + 1][column].bomb_location){
+        
+                new_game[row + 1][column].selected_square = true;
+        
+            }else{
+        
+                adjacent_mine_count++;
+            };
+    
+            if (!new_game[row][column - 1].bomb_location){
+        
+                new_game[row][column - 1].selected_square = true;
+        
+            }else{
+        
+                adjacent_mine_count++;
+        
+            }
+    
+            if (!new_game[row - 1][column - 1].bomb_location){
+        
+                new_game[row - 1][column - 1].selected_square = true;
+        
+            }else{
+        
+                adjacent_mine_count++;
+            }
+    
+            if (!new_game[row + 1][column - 1].bomb_location){
+        
+                new_game[row + 1][column -1].selected_square = true;
+            }
+    
+            if (adjacent_mine_count > 0){
+        
+                new_game[row][column].neighbor_mine = adjacent_mine_count;
+        
+            }
+        }
+        
+        else{
+    
+            if (!new_game[row - 1][column].bomb_location){
+        
+                new_game[row - 1][column].selected_square = true;
+        
+            }else{
+        
+                adjacent_mine_count++;
+            };
+    
+            if (!new_game[row + 1][column].bomb_location){
+        
+                new_game[row + 1][column].selected_square = true;
+        
+            }else{
+        
+                adjacent_mine_count++;
+            };
+    
+            if (!new_game[row][column + 1].bomb_location){
+        
+                new_game[row][column + 1].selected_square = true;
+        
+            }else{
+        
+                adjacent_mine_count++;
+        
+            }
+    
+            if (!new_game[row][column - 1].bomb_location){
+        
+                new_game[row][column - 1].selected_square = true;
+        
+            }else{
+        
+                adjacent_mine_count++;
+            }
+    
+            if (!new_game[row - 1][column + 1].bomb_location){
+        
+                new_game[row - 1][column + 1].selected_square = true;
+            }else{
+                
+                adjacent_mine_count++;
+            }
+            
+            if (!new_game[row - 1][column - 1].bomb_location){
+                
+                new_game[row - 1][column - 1].selected_square = true;
+            
+            }else{
+                
+                adjacent_mine_count++;
+            }
+            
+            if (! new_game[row + 1][column + 1].bomb_location){
+                
+                new_game[row + 1][column + 1].selected_square = true;
+            }else{
+                
+                adjacent_mine_count++;
+                
+            }
+            
+            if (!new_game[row + 1][column - 1].bomb_location){
+                
+                new_game[row + 1][column - 1].selected_square = true;
+                
+            }else{
+                
+                adjacent_mine_count++;
+            }
+    
+            if (adjacent_mine_count > 0){
+        
+                new_game[row][column].neighbor_mine = adjacent_mine_count;
+        
+            }
+        }
+    }
+}
