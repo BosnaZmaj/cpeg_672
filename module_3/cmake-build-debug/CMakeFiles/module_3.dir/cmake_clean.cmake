22491c269b61c2c3ee7e9d948623b1fd34bca903
BosnaZmaj <indysubasic@gmail.com>
2020-05-08 11:46:44 -0400

    Initial Commit

---
+++ module_3/cmake-build-debug/CMakeFiles/module_3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/module_3.dir/vul.c.obj"
+  "libmodule_3.dll.a"
+  "module_3.exe"
+  "module_3.exe.manifest"
+  "module_3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/module_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ module_3/vul.c
@@ -0,0 +1,15 @@
+//
+// Created by BosnaZmaj on 5/8/2020.
+//
+
+#include <string.h>
+
+void go(char *data) {
+    char name[64];
+
+    strcpy(name, data);
+}
+
+int main(int argc, char **argv) {
+    go(argv[1]);
+}
